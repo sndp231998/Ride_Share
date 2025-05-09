caf0e582a67d479bf7695e15630de1ba6476cf7c
sndp231998 <sandipchapagain797@gmail.com>
2025-05-09 17:12:17 +0545

    firebase error

--- src/main/java/com/ride_share/config/FirebaseConfig.java
+++ src/main/java/com/ride_share/config/FirebaseConfig.java
@@ -2,7 +2,7 @@ package com.ride_share.config;
 
 import java.io.FileInputStream;
 import java.io.IOException;
-
+import java.io.InputStream;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,7 +18,7 @@ public class FirebaseConfig {
 
     @Bean
     public FirebaseMessaging firebaseMessaging() throws IOException {
-        FileInputStream serviceAccount = new FileInputStream("src/main/resources/serviceAccountKey.json");
+        InputStream serviceAccount = getClass().getClassLoader().getResourceAsStream("serviceAccountKey.json");
 
         FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
